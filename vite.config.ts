87ac295740505779c481ab03637f27d74b5df49d
JustinS <jusyers25@gmail.com>
2025-12-06 19:50:09 -0500

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Songbook/',
+  base: '/songbook-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
